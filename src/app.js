b94c30c72d6ad495bee4ebb27de03e6ae0a9df2a
Wagner-Wagner <dewilde.olivier@outlook.com>
2023-05-16 14:51:42 +0200

    JS forEach sur les image pour ajouter les icon de l API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
+// URL & KEY = 'http://api.weatherapi.com/v1/current.json?key=6c7a03f5f1274291aaf135608230505&q=London&aqi=no'
+
 // Function search
-const url = "http://api.weatherapi.com/v1/current.json?key= 6c7a03f5f1274291aaf135608230505&q="
 const searchBtn = document.querySelector('.btn-search');
 const inputSearch =  document.querySelector('.input-search');
 let locationName = document.querySelector('.location');
 const degreDay = document.querySelector('.degre-day');
-const conditionText = document.querySelector('.condition-text')
-let weatherImg = document.querySelector('.weather-img')
+const conditionText = document.querySelector('.condition-text');
+let weatherImg = document.querySelector('.weather-img');
+let time =  document.querySelector('.time');
+const blockImg = document.querySelectorAll('.block-img img')
+//blockImg.forEach(element => console.log(element));
+
 
 
 searchBtn.addEventListener('click', search);
@@ -15,20 +20,44 @@ function search() {
     }else{
         let inputSearchValue = inputSearch.value;
         locationName.textContent = inputSearchValue;
+        
 
 
-        fetch(url + inputSearchValue +"&aqi=no")
+        fetch(`http://api.weatherapi.com/v1/current.json?key=6c7a03f5f1274291aaf135608230505&q=${inputSearchValue}&aqi=no`)
         .then((resp) => resp.json())
         .then(function(data) {
             let respData = data.current;
-            console.log(respData);
             degreDay.textContent = respData.temp_c
             conditionText.textContent = respData.condition.text
             weatherImg.src = respData.condition.icon
+            let timeData = data.location.localtime
+            time.textContent = data.location.localtime
+            /*
+            console.log(respData);
+            console.log(timeData);
+            console.log(data);
+            */
+
+
+            fetch(`http://api.weatherapi.com/v1/forecast.json?key=6c7a03f5f1274291aaf135608230505&q=${inputSearchValue}&days=1&aqi=no&alerts=no`)
+            .then((resp) => resp.json())
+            .then(function (data) {
+                let dateData = data.forecast.forecastday[0].hour;
+                for (let i = 6; i < dateData.length; i+=4) {
+                    blockImg.forEach(element => element.src =dateData[i].condition.icon )
+                
+                    console.log(dateData[i]);
+                    //console.log(dateData[i].condition);
+                }
+                
+            })
         }
         );
-      
+
+        inputSearch.value = "";
     }
+
+   
     
 }
 
@@ -36,7 +65,7 @@ function search() {
 
 
 // URL + KEY
-//'http://api.weatherapi.com/v1/current.json?key=6c7a03f5f1274291aaf135608230505&q=London&aqi=no'
+
 
 
 
